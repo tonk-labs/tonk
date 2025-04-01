24ad94fd1bd3735a42cc59140005c53fdb7efd0f
Jack D. Douglas <cincomc@proton.me>
2025-04-01 18:22:31 +0100

    feat(create): create integration from template (#61)

--- packages/create/src/types.ts
+++ packages/create/src/types.ts
@@ -3,10 +3,10 @@ export interface ProjectPlan {
   projectDescription: string;
 }
 
-export type TemplateType = "default" | "React-PWA";
+export type TemplateType = "default" | "React-PWA" | "react" | "integration";
 
 export interface TemplateConfig {
   name: string;
   plan: ProjectPlan;
   template: TemplateType;
-} 
+}
