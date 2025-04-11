37a9b6c91c64b0ce89c337fed2d24d99d1f54f41
Jack D. Douglas <cincomc@proton.me>
2025-04-11 10:18:46 +0100

    fix(hub): don't display non-automerge files

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -38,9 +38,6 @@ export const devCommand = new Command('dev')
       mode: 'development',
       verbose: true,
       distPath: undefined,
-      apiProxy: {
-        target: 'http://localhost:6080/api',
-      },
     });
 
     // Start webpack dev server
