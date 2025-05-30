4a633b17533ed3c5c95ff2cf2a5e367363148aaa
Jack D. Douglas <cincomc@proton.me>
2025-05-30 12:58:20 +0200

    feat(examples): add raw content toggle

--- examples/file-browser/dev-dist/sw.js
+++ examples/file-browser/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qanv3uik8mg"
+    "revision": "0.r157cib0q68"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
