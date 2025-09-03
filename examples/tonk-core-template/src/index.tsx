881fb6485dcc94ea28fe0e81ac308c6d53f49b69
Jack D. Douglas <cincomc@proton.me>
2025-09-03 12:16:38 +0100

    feat(examples): TON-1423: using tonk core

---
+++ examples/tonk-core-template/src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { createSyncEngine } from '@tonk/core';
+
+const engine = await createSyncEngine();
+const vfs = await engine.getVfs();
+
+await vfs.createFile('/hello.txt', 'Hello, World!');
+const content = await vfs.readFile('/hello.txt');
+console.log('File content:', content);
+
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find the root element');
+const root = createRoot(container);
+
+const basename =
+  import.meta.env.VITE_BASE_PATH !== '/'
+    ? import.meta.env.VITE_BASE_PATH?.replace(/\/$/, '')
+    : '';
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
