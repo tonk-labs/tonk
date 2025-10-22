69b36442ed564b8d0d3513119f08df8aed1a17ba
Alexander Mangel <cygnusfear@gmail.com>
2025-10-22 16:52:43 +0200

    chore: the great example exodus
    Signed-off-by: Alexander Mangel <cygnusfear@gmail.com>

--- examples/demo/app/src/main.tsx
+++ examples/demo/app/src/main.tsx
@@ -1,11 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { install } from "@twind/core";
-import config from "../twind.config.js";
 import "./index.css";
 import App from "./App.tsx";
-
-install(config);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
