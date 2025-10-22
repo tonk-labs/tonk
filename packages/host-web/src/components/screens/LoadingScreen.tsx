cbf3f9578ce41deade660d8ae61b9c62ba7fb8f6
Alexander Mangel <cygnusfear@gmail.com>
2025-10-22 13:36:05 +0200

    wip: screen
    Signed-off-by: Alexander Mangel <cygnusfear@gmail.com>

--- packages/host-web/src/components/screens/LoadingScreen.tsx
+++ packages/host-web/src/components/screens/LoadingScreen.tsx
@@ -1,12 +1,16 @@
 import { useTonk } from '../../context/TonkContext';
-import icon_hourglass from "../../assets/icon-small-hourglass.svg";
+import icon_hourglass from '../../assets/icon-small-hourglass.svg';
 
 export function LoadingScreen() {
   const { loadingMessage } = useTonk();
 
   return (
-    <div class="border-1 border-[#000] flex min-h-0 h-full w-full grow items-center justify-center bg-[#E0E0E0]">
-        <img src={icon_hourglass} alt={loadingMessage} class="w-8 h-8 mx-auto animate-spin"/>
+    <div class="border-1 border-[#000] flex min-h-0 h-full w-full grow items-center justify-center bg-[#E0E0E0] animate-pulse">
+      <img
+        src={icon_hourglass}
+        alt={loadingMessage}
+        class="w-8 h-8 mx-auto animate-spin"
+      />
     </div>
   );
 }
--- packages/host-web/src/components/screens/SplashScreen.tsx
+++ packages/host-web/src/components/screens/SplashScreen.tsx
@@ -1,9 +1,13 @@
-import icon_tonk from "../../assets/icon-tonk.svg";
+import icon_tonk from '../../assets/icon-tonk.svg';
 
 export function SplashScreen() {
   return (
-    <div class="flex min-h-0 h-full w-full grow items-center justify-center bg-[#E0E0E0]">
-        <img src={icon_tonk} alt="tonk icon" class="w-8 h-8 mx-auto"/>
+    <div class="border-1 border-[#000] flex min-h-0 h-full w-full grow items-center justify-center bg-[#E0E0E0]">
+      <img
+        src={icon_tonk}
+        alt={'Tonk'}
+        class="w-8 h-8 mx-auto animate-bounce"
+      />
     </div>
   );
 }
