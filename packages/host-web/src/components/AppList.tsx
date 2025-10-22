797f840ca478d38ace5962fd9090a78e56d8cff5
Alexander Mangel <cygnusfear@gmail.com>
2025-10-22 13:51:43 +0200

    feat: bang
    Signed-off-by: Alexander Mangel <cygnusfear@gmail.com>

--- packages/host-web/src/components/AppList.tsx
+++ packages/host-web/src/components/AppList.tsx
@@ -1,12 +1,14 @@
 import { useTonk } from '../context/TonkContext';
 import { useDialogs } from '../context/DialogContext';
-import icon_upload from "../assets/icon-upload.svg";
+import icon_upload from '../assets/icon-upload.svg';
 import { useEffect } from 'preact/hooks';
-import icon_file from "../assets/icon-tonk.svg";
+import icon_file from '../assets/icon-tonk.svg';
+import { useServiceWorker } from '../hooks/useServiceWorker';
 
 export function AppList() {
   const { availableApps, selectedAppIndex, setSelectedAppIndex } = useTonk();
   const { openConfirmationDialog } = useDialogs();
+  const { confirmBoot } = useServiceWorker();
 
   const handleAppClick = (index: number) => {
     if (index >= 0 && index < availableApps.length) {
@@ -16,20 +18,25 @@ export function AppList() {
       openConfirmationDialog(appName);
     }
   };
+  const bootTheApp = async (index: number) => {
+    await confirmBoot(availableApps[0].name);
+  };
 
   if (availableApps.length === 0) {
     return (
       <ul class="list-none flex items-center justify-center w-full">
         <div class="flex flex-col">
-        <img src={icon_upload} alt="upload icon" class="w-16 h-16 mx-auto"/>
-        <p>Drag a .tonk file to load it.</p></div>
+          <img src={icon_upload} alt="upload icon" class="w-16 h-16 mx-auto" />
+          <p>Drag a .tonk file to load it.</p>
+        </div>
       </ul>
     );
   }
+
   useEffect(() => {
-  if (availableApps.length > 0) {
-    handleAppClick(0);
-  }
+    if (availableApps.length > 0) {
+      bootTheApp(0);
+    }
   }, [availableApps.length]);
 
   return (
@@ -43,14 +50,16 @@ export function AppList() {
         return (
           <li
             key={appName}
-            onClick={() => handleAppClick(index)}
+            onClick={() => bootTheApp(index)}
             class={`border border-black px-3 py-2 cursor-pointer flex items-center text-black font-semibold transition-colors ${
               isSelected
                 ? 'bg-[#ddd]/30 text-black'
                 : 'bg-transparent hover:bg-[#333]'
             }`}
           >
-            <span><img src={icon_file} alt="app icon" class="w-6 h-6 mr-2"/></span>
+            <span>
+              <img src={icon_file} alt="app icon" class="w-6 h-6 mr-2" />
+            </span>
             <span class="pl-4 text-[6pt]">{appStatus}</span>
           </li>
         );
