2b887c419bb0f7077448a3bb3f962faca621e21c
Alexander Mangel <cygnusfear@gmail.com>
2025-10-23 11:15:25 +0200

    fix: 🔬⚡ Resolve TypeScript build errors and enable demo app bundling (#306)
    * fix: bundle script for examples/demo
    
    Signed-off-by: Alexander Mangel <cygnusfear@gmail.com>
    
    * fix: not prod
    
    Signed-off-by: Alexander Mangel <cygnusfear@gmail.com>
    
    ---------
    
    Signed-off-by: Alexander Mangel <cygnusfear@gmail.com>

--- examples/demo/app/src/hooks/useVFS.ts
+++ examples/demo/app/src/hooks/useVFS.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import { getVFSService } from '../lib/vfs-service';
 
 type ConnectionState =
