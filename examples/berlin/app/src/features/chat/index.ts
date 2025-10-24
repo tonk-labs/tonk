e18bb9743a3379d785ed05a68d2087b3917e61bb
Alexander Mangel <cygnusfear@gmail.com>
2025-10-24 16:44:57 +0200

    feat(chat): add feature exports

---
+++ examples/berlin/app/src/features/chat/index.ts
@@ -0,0 +1,3 @@
+export { ChatWindow } from './components/ChatWindow';
+export { useChat } from './stores/chatStore';
+export type { ChatMessage, ChatReaction, WindowState, ChatConfig } from './types';
