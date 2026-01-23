2f30f7f25710960d8ffa7071bd0ff927955e6c59
Irakli Gozalishvili <contact@gozala.io>
2026-01-23 15:02:20 -0800

    chore: update dialog

--- nix/rust.nix
+++ nix/rust.nix
@@ -18,8 +18,8 @@ let
   # that the same dependencies can be used across all derivations that
   # need them. Crane expects the full git URL as the key.
   cargoGitDependencies = {
-    "git+https://github.com/dialog-db/dialog-db.git?branch=ucan-provider-caps#9cf8915f12047c765183725be1ad42016599523f" =
-      "sha256-MyQRzVLpaSehtPOHhNoGHw+3qItQPmFypnoRPt/tbiM=";
+    "git+https://github.com/dialog-db/dialog-db.git?branch=ucan-provider-caps#f630cc278dcde4f54890a7224edac7148070f98d" =
+      "sha256-AWcT7T3wCoKh/ZaUw220LORKzl9VtG1D5wX3sCPsyOI=";
     "git+https://github.com/tonk-labs/rs-ucan.git?branch=jackddouglas/feat/check#671a0256621eb4656b42d9e631108da3ec18158b" =
       "sha256-5KQ7wIXv7PHgd6y1pq0+aUU/VFW7BLxECmVUNk1JfGw=";
   };
