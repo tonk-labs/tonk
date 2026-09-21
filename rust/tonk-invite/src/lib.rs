99e60c4c0f6a09acd497af1c026ad36cb321ecee
Jack D. Douglas <jack@jackddouglas.com>
2026-09-21 18:24:22 +0100

    [1/9] Define scoped invitations and signed local-space consent (#976)

--- rust/tonk-invite/src/lib.rs
+++ rust/tonk-invite/src/lib.rs
@@ -41,6 +41,8 @@
 //! [`Subject::Specific`]: dialog_ucan_core::subject::Subject::Specific
 //! [`Subject::Any`]: dialog_ucan_core::subject::Subject::Any
 
+pub mod connection;
+pub mod local_space_link;
 pub mod shortcut;
 
 use anyhow::{Context, Result};
